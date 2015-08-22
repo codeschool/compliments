a60ce6ed9febc66d8266f5a2b0a69db39217603e
Drew Barontini <drewbarontini@gmail.com>
2015-08-22 15:08:31 -0400

    Add quote CRUD operations, views

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,53 @@
 class QuotesController < ApplicationController
+  before_action :find_quote, only: [:show, :edit, :update, :destroy]
+
   def index
-    Quote.all.order('created_at DESC')
+    @quotes = Quote.all
+  end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(quote_params)
+
+    if @quote.save
+      redirect_to quotes_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @quote.update_attributes(quote_params)
+      redirect_to quotes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @quote.destroy
+      redirect_to quotes_path
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def find_quote
+    @quote = Quote.find(params[:id])
+  end
+
+  def quote_params
+    params.require(:quote).permit(:user_id, :text)
   end
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,9 @@
 class Quote < ActiveRecord::Base
   belongs_to :user
+
+  default_scope { order("created_at DESC") }
+
+  def from?(user)
+    user_id == user.id
+  end
 end
