71923a205d957a71da62300dfb1fe9dca176990f
Drew Barontini <drewbarontini@gmail.com>
2015-09-05 14:12:23 -0400

    Add quotes theme

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def active_class(path)
     "is-active" if current_page?(path)
   end
+
+  def body_class
+    controller.controller_name
+  end
+
+  def compliments?
+    controller.controller_name == 'compliments'
+  end
 end
