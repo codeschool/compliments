ae38058fc4b88a4e910ee6bafcec8c2288c7738e
Drew Barontini <drewbarontini@gmail.com>
2015-09-07 09:59:10 -0400

    Add 'quoter_id' column to 'quotes'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150828201512) do
+ActiveRecord::Schema.define(version: 20150907135356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20150828201512) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "quoter_id"
   end
 
   add_index "quotes", ["user_id"], name: "index_quotes_on_user_id", using: :btree
