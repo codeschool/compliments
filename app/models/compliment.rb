9af0e346ebea2f83194f75083d96e1b85ea0a108
Katie Delfin <katiedelfin@gmail.com>
2017-02-23 14:37:25 -0500

    Only include compliments from the last month on the tv

--- app/models/compliment.rb
+++ app/models/compliment.rb
@@ -33,15 +33,11 @@ class Compliment < ActiveRecord::Base
   end
 
   def self.recent
-    where("compliments.created_at > ?", 2.weeks.ago)
+    where("compliments.created_at > ?", 1.month.ago)
   end
 
   def self.random
-    if [true, false].sample
-      unscoped.active.recent.order("RANDOM()").first
-    else
-      unscoped.active.order("RANDOM()").first
-    end
+    unscoped.active.recent.order("RANDOM()").first
   end
 
   def from?(user)
