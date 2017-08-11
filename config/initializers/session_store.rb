5792d8dc920c3d35f9d19ef8c50fd3441d0a79d4
Olivier Lacan <hi@olivierlacan.com>
2017-08-11 18:51:29 +0200

    Don't include protocol and trailing slash

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
 # Be sure to restart your server when you modify this file.
-Rails.application.config.session_store :cookie_store, key: '_compliments_session', domain: ENV["APP_DOMAIN"]
+Rails.application.config.session_store :cookie_store, key: '_compliments_session', domain: ENV["APP_DOMAIN"].sub("http://", "").sub("/","")
